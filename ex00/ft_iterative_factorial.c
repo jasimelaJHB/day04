ad301dd1d7adf269e7437c85af0a4006ed108230
Jabulile Simelane <jasimela@c5r6s7.wethinkcode.co.za>
2020-06-25 10:02:47 +0200

    ex00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jasimela <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 09:42:15 by jasimela          #+#    #+#             */
+/*   Updated: 2020/06/25 10:01:15 by jasimela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>
+
+/*void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+*/ 
+int ft_iterative_factorial(int nb)
+{
+	int i;
+	int fact;
+	int num = nb;
+
+	fact = 1;
+	if(nb <= 0)
+		fact = 1;
+	else
+	{
+		i = 1;
+		while(i <= nb)
+		{
+			fact = fact * i;
+			putchar(i++);
+		}
+	}
+	return fact;
+}
+
+/*int main()
+{
+	int num;
+	num = ft_iterative_factorial(10);
+	return (0);
+\*}
